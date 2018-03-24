7bb1fa5e8ea4c429ec35a4d3f39b1112a806ce58
astudymanwbq <1282656881@qq.com>
2018-03-24 13:15:08 +0800

    Revert "Signed-off-by: astudymanwbq <1282656881@qq.com>"
    This reverts commit 7629de3c15761b41dec00773866300d6aabfe40a.

--- douban/src/test/com/wbq/dao/SplitUtil.java
+++
@@ -1,9 +0,0 @@
-package com.wbq.dao;
-
-public class SplitUtil {
-    public static void main(String[] args) {
-       String a= "https://movie.douban.com/subject/26004132/?from=playing_poster".split("from=playing_poster")[0];
-       String b=a.substring(0,a.length()-1);
-       System.out.print(b.toString());
-    }
-}
